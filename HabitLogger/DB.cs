0035254a95a1def9d7f885d70feab3b7146bcbe7
Dr0pyyyy <adamkoutny12345@seznam.cz>
2023-05-06 14:56:30 +0200

    Add project files.

---
+++ HabitLogger/DB.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitLogger
+{
+    internal class DB
+    {
+        public SqlConnection Connection { get; set; }
+        public string ConnectionString { get; set; }
+
+        public DB() {
+            ConnectionString = "Data Source=DESKTOP-NJS96BC\\MSSQLSERVER01;Initial Catalog=test;Integrated Security=SSPI;";
+            Connection = new SqlConnection(ConnectionString);
+            Connection.Open();
+            Console.WriteLine("Database loaded successfully!"); //TODO instead of cw learn to use Ilogger
+            Connection.Close();
+        }
+
+        public void ShowMenu()
+        {
+            Console.WriteLine(
+                "------------------------\n" +
+                "Input your command\n" +
+                "\n" +
+                "\n" +
+                "U to update hours\n" +
+                "D to delete date\n" +
+                "I to insert date\n" +
+                "V to view data\n" +
+                "S to show menu\n" +
+                "0 to exit\n" +
+                "------------------------"
+            );
+        }
+
+        public void InsertNewRecord() { 
+            Connection.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO tableTest VALUES('aaaa')", Connection);
+            cmd.ExecuteNonQuery();
+            Connection.Close();
+            Console.WriteLine("Value inserted successufully!");  //TODO instead of cw learn to use Ilogger
+        }
+
+        public void ViewDatabase() {
+            string output = "";
+            Connection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tableTest", Connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                output += reader.GetValue(0);
+            }
+            Connection.Close();
+            Console.WriteLine(output); //TODO instead of cw learn to use Ilogger
+        }
+
+        public void DeleteData() {
+            Connection.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM tableTest WHERE column1='aaaa'", Connection);
+            cmd.ExecuteNonQuery();
+            Connection.Close();
+            Console.WriteLine("Data deleted successfully!"); //TODO instead of cw learn to use Ilogger
+        }
+
+        public void UpdateData() {
+            Connection.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE tableTest set column1 = 'adamececek' where column1 = 'aaaa'", Connection);
+            cmd.ExecuteNonQuery();
+            Connection.Close();
+            Console.WriteLine("Data updated successfully!"); //TODO instead of cw learn to use Ilogger
+        }
+    }
+}
---
+++ HabitLogger/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace HabitLogger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DB db = new DB();
+
+            db.ShowMenu();
+
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                switch (userInput.ToUpper())
+                {
+                    case "U":
+                        db.UpdateData();
+                        break;
+                    case "D":
+                        db.DeleteData();
+                        break;
+                    case "I":
+                        db.InsertNewRecord();
+                        break;
+                    case "V":
+                        db.ViewDatabase();
+                        break;
+                    case "0":
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Error: Invalid input!"); //TODO instead of cw learn to use Ilogger
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
